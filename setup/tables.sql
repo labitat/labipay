aaee9fe8845c9a51191e5d262d117c276d9588d0
Jørgen Elgaard Larsen <jel@elgaard.net>
2011-01-02 22:47:46 +0100

    Moved constraints from each attribute line to the bottom og each create statement - apparently, MySQL ignores the former syntax

--- setup/tables.sql
+++ setup/tables.sql
@@ -1,6 +1,9 @@
 
 DROP TABLE IF EXISTS person, account, card, transfer, shop, shop_admin, category, product, purchase, terminal, terminal_group, terminal_group_member, sold_by CASCADE;
 
+SET storage_engine=INNODB;
+
+
 CREATE TABLE person (
        id    	      int		primary key auto_increment,
        name  	      varchar(255)	null,
@@ -10,42 +13,50 @@ CREATE TABLE person (
 
 CREATE TABLE account (
        number	      int		primary key auto_increment,
-       owner	      int		not null references person (id),
+       owner	      int		not null,
        name	      varchar(255)	not null default 'Unnamed account',
-       balance	      decimal(10,2)	not null default 0
+       balance	      decimal(10,2)	not null default 0,
+
+       CONSTRAINT FOREIGN KEY (owner) REFERENCES person (id)
 );
 
 
 CREATE TABLE card (
        id	      int		primary key auto_increment,
-       account	      int		not null references account (number),
+       account	      int		not null,
        hash	      varchar(256)	not null default '',
        name	      varchar(255)	not null default 'Unnamed card',
 
-       INDEX (hash)
+       INDEX (hash),
+       CONSTRAINT FOREIGN KEY (account) REFERENCES account (number)
 );
 
 
 CREATE TABLE transfer (
        id    	      int		primary key auto_increment,
        time	      timestamp		not null default CURRENT_TIMESTAMP,
-       from_account   int		null comment 'Account number, NULL for cash' references account (number),
-       to_account     int               null comment 'Account number, NULL for cash' references account (number),
+       from_account   int		null comment 'Account number, NULL for cash',
+       to_account     int               null comment 'Account number, NULL for cash',
        amount	      decimal(10,2)	not null default 0,
 
        INDEX (from_account),
        INDEX (to_account),
-       INDEX (time)
+       INDEX (time),
+       CONSTRAINT FOREIGN KEY (from_account) REFERENCES account (number),
+       CONSTRAINT FOREIGN KEY (to_account) REFERENCES account (number)
 );
 
 
 CREATE TABLE shop (
        id	      int		primary key auto_increment,
        name	      varchar(255)	not null,
-       account	      int		not null references account (number),
-       gamble_account int		null references account (number),
+       account	      int		not null,
+       gamble_account int		null,
 
-       INDEX (name)
+       INDEX (name),
+       CONSTRAINT FOREIGN KEY (account) REFERENCES account (number),
+       CONSTRAINT FOREIGN KEY (gamble_account) REFERENCES account (number)
+
 );
 
 
@@ -53,26 +64,29 @@ CREATE TABLE shop_admin (
        shop  	      int               not null references shop (id),
        person	      int		not null references person (id),
        
-       PRIMARY KEY (shop, person)
+       PRIMARY KEY (shop, person),
+       CONSTRAINT FOREIGN KEY (shop) REFERENCES shop (id),
+       CONSTRAINT FOREIGN KEY (person) REFERENCES person (id)
 );
 
 
 CREATE TABLE category (
        id    	      int		primary key auto_increment,
        name	      varchar(255)	not null default 'Unnamed',
-       parent	      int		null references category (id),
+       parent	      int		null,
        
-       INDEX (parent)
+       INDEX (parent),
+       CONSTRAINT FOREIGN KEY (parent) REFERENCES parent (id)
 );
 
 
 CREATE TABLE product (
        id    	      int		primary key auto_increment,
-       shop	      int		not null references shop (id),
+       shop	      int		not null,
        name	      varchar(255)	not null,
        ean	      int		null,
        unit	      varchar(20)	not null default 'piece',
-       category	      int		not null references category (id),
+       category	      int		not null,
        price	      decimal(10,5)	not null comment 'Price per unit',
        int_amount     bit		not null default 1 comment 'Is purchase amount restricted to integer amounts (e.g. pieces)?',
        stock          decimal(10,5)	not null default 0,
@@ -84,19 +98,22 @@ CREATE TABLE product (
        INDEX (ean),
        INDEX (category),
        INDEX (shop),
-       INDEX (name)
+       INDEX (name),
+       CONSTRAINT FOREIGN KEY (shop) REFERENCES shop (id),
+       CONSTRAINT FOREIGN KEY (category) REFERENCES category (id),
 );
 
 
 CREATE TABLE purchase (
        id    	      int		primary key auto_increment,
-       product	      int		not null references product (id),
+       product	      int		not null,
        time	      timestamp		not null default CURRENT_TIMESTAMP,
        amount	      decimal(10,3)	not null,
        price	      decimal(10,2)	not null,
 
        INDEX (product),
-       INDEX (time)
+       INDEX (time),
+       CONSTRAINT FOREIGN KEY (product) REFERENCES product (id),
 );
 
 
@@ -117,17 +134,20 @@ CREATE TABLE terminal_group (
 );
 
 CREATE TABLE terminal_group_member (
-       terminal	      int	        references terminal (id),
-       terminal_group int		references terminal_group (id),
+       terminal	      int,
+       terminal_group int,
 
-       PRIMARY KEY (terminal, terminal_group)
+       PRIMARY KEY (terminal, terminal_group),
+       CONSTRAINT FOREIGN KEY (terminal) REFERENCES terminal (id),
+       CONSTRAINT FOREIGN KEY (terminal_group) REFERENCES terminal_group (id)
 );
 
 
 CREATE TABLE sold_by (
-       product	      int		references product (id),
-       terminals      int		references terminal_group (id),
+       product	      int,
+       terminals      int,
 
-       PRIMARY KEY (product, terminals)
+       PRIMARY KEY (product, terminals),
+       CONSTRAINT FOREIGN KEY (product) REFERENCES product (id),
+       CONSTRAINT FOREIGN KEY (terminals) REFERENCES terminal_group (id)
 );  
-
